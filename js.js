b1aff80d735afbd554f75eca95c0a625b21762d1
sandrabirkefeldt <56107469+sandrabirkefeldt@users.noreply.github.com>
2021-05-10 15:10:18 +0200

    Version 9

--- js.js
+++ js.js
@@ -15,3 +15,29 @@ function togglePopup() {
     document.getElementById("popup-1").classList.toggle("active");
 }
 
+//Dropdowns
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+function rotateFunction() {
+    document.getElementById("arrowDropdown").classList.toggle("rotateArrow");
+}
+
--- leaflet.js
+++ leaflet.js
@@ -8,29 +8,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1Ijoic2FuZHJhYmlya2UiLCJhIjoiY2tueWI2cnBpMDU1aTJxcmpkZGRlN3A1MiJ9.9YHT3VTXpPmzcWZHe3upgw'
 }).addTo(mymap);
-/*
-//Den Gamle By
-var denGamleBy = L.marker([56.1587826, 10.1899265]).addTo(mymap);
-denGamleBy.bindPopup("<b>Den Gamle By</b><br>I am a popup.").openPopup();
-
-//Salling ROOFTOP
-var sallingRooftop = L.marker([56.1543098,10.2050384]).addTo(mymap);
-sallingRooftop.bindPopup("<b>Salling ROOFTOP</b><br>I am a popup.").openPopup();
-
-//AROS
-var aros = L.marker([56.153919,10.1975273]).addTo(mymap);
-aros.bindPopup("<b>ARoS</b><br>I am a popup.").openPopup();
-
-//Byen ved bugten
-var byenVedBugten = L.marker([56.164897, 10.224258]).addTo(mymap);
-byenVedBugten.bindPopup("<b>Byen ved bugten</b><br>I am a popup.").openPopup();
-*/
 
 var LeafIcon = L.Icon.extend({
     options: {
-        iconSize:     [30, 41.82],
-        iconAnchor:   [22, 94],
-        popupAnchor:  [-3, -76]
+        iconSize:     [20, 28.28],
+        iconAnchor:   [20, -10],
+        popupAnchor: [5, -5]
     }
 });
 
@@ -38,16 +21,28 @@ var brunPin = new LeafIcon({iconUrl: 'img/brun-pin.png'}),
     blaaPin = new LeafIcon({iconUrl: 'img/blaa-pin.png'}),
     gronPin = new LeafIcon({iconUrl: 'img/gron-pin.png' });
     gulPin = new LeafIcon({iconUrl: 'img/gul-pin.png' });
-    rodPin = new LeafIcon({iconUrl: 'img/rod-pin.png' });
+rodPin = new LeafIcon({ iconUrl: 'img/rod-pin.png' });
+    sortPin = new LeafIcon({ iconUrl: 'img/sort-pin.png' });
 
 L.icon = function (options) {
     return new L.Icon(options);
 };
 
-L.marker([56.1587826, 10.1899265], {icon: brunPin}).addTo(mymap).bindPopup("<strong>Den Gamle By</strong>");
+
+L.marker([56.1587826, 10.1899265], {icon: brunPin}).addTo(mymap).bindPopup("<h2>Den Gamle By</h2> <p>Med over en halv mio. årlige gæster er Den Gamle By en af Aarhus’ mest kendte og elskede kulturinstitutioner. Det gør Den Gamle By til et oplagt sted for virksomheder, forskere og formidlere, der har en historisk vinkel på fødevarer og madkultur at samle aarhusianerne om spændende madaktiviteter.</p>");
 L.marker([56.1543098,10.2050384], {icon: gulPin}).addTo(mymap).bindPopup("<strong>Salling ROOFTOP</strong>");
 L.marker([56.153919,10.1975273], {icon: rodPin}).addTo(mymap).bindPopup("<strong>ARoS</strong>");
-L.marker([56.164897, 10.224258], {icon: blaaPin}).addTo(mymap).bindPopup("<strong>Byen ved bugten</strong>");
+L.marker([56.165087650755915, 10.222491152274102], {icon: blaaPin}).addTo(mymap).bindPopup("<strong>Kystpromenaden</strong>");
+L.marker([56.16382724715992, 10.226949940784534], {icon: blaaPin}).addTo(mymap).bindPopup("<strong>Spring ombord på fiskekutteren Tove</strong>");
+L.marker([56.16776917270664, 10.226445581115856], {icon: blaaPin}).addTo(mymap).bindPopup("<strong>Byens længste ta’-selv-bord</strong>");
+L.marker([56.15883167094611, 10.137131214746638], {icon: sortPin}).addTo(mymap).bindPopup("<strong>Fællesmiddag i Gellerup</strong>");
+L.marker([56.151869450501536, 10.202816896891779], {icon: sortPin}).addTo(mymap).bindPopup("<strong>Fællesmiddag i Rådhustårnet</strong>");
+L.marker([56.165222218860464, 10.226224425728649], {icon: sortPin}).addTo(mymap).bindPopup("<strong>Fællesmiddag i Generationernes hus</strong>");
+L.marker([56.16671614197104, 10.21550224138891], {icon: gronPin}).addTo(mymap).bindPopup("<strong>Food Walks</strong>");
+L.marker([56.16707407657883, 10.193216065246046], {icon: gronPin}).addTo(mymap).bindPopup("<strong>Food Walks</strong>");
+L.marker([56.158542394000655, 10.215716531544128], {icon: gronPin}).addTo(mymap).bindPopup("<strong>Food Walks</strong>");
+L.marker([56.15293312277212, 10.209716407197975], {icon: gronPin}).addTo(mymap).bindPopup("<strong>Food Walks</strong>");
+L.marker([56.17375491026483, 10.226109604072292], {icon: gronPin}).addTo(mymap).bindPopup("<strong>Food Walks</strong>");
 
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
